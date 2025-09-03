ec7d2d736c6fe9f452ae503ba416773ea781a220
Thiago Hernandez <thiago.hernandez@gmail.com>
2025-09-03 21:44:36 +0200

    Feat: Changed the files to deploy on cpanel

---
+++ app.js
@@ -0,0 +1,31 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000; // cPanel/Passenger sets PORT
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query);
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query);
+      } else {
+        await handle(req, res, parsedUrl);
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('Internal server error');
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
+  });
+});
